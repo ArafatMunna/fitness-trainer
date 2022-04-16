e54b287b527d1aebfceef2c434959fa62a86b111
ArafatMunna <arafatmunna000@gmail.com>
2022-04-17 02:34:32 +0600

    logout toggle added in header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    };
     return (
         <Navbar
             collapseOnSelect
@@ -24,9 +32,18 @@ const Header = () => {
                         <Nav.Link as={Link} to="/about">
                             About
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/login">
-                            Login
-                        </Nav.Link>
+                        {user ? (
+                            <button
+                                className="btn bnt-link text-white text-decoration-none"
+                                onClick={handleSignOut}
+                            >
+                                Logout
+                            </button>
+                        ) : (
+                            <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
